194d5970f49dbed9d3dba32490ec4713a439e6aa
focus617 <13980668829@163.com>
2022-09-10 15:45:52 +0800

    Refactor ProductController to seperate ProductService outside.

--- src/main/kotlin/com/focus617/webbackendspringboot/controller/ProductController.kt
+++ src/main/kotlin/com/focus617/webbackendspringboot/controller/ProductController.kt
@@ -1,64 +1,31 @@
 package com.focus617.webbackendspringboot.controller
 
-import com.focus617.webbackendspringboot.data.dtos.PaginatedResponse
+import com.focus617.webbackendspringboot.domain.interactors.ProductService
 import com.focus617.webbackendspringboot.domain.model.Product
-import com.focus617.webbackendspringboot.domain.repository.ProductRepository
-import org.springframework.data.domain.PageRequest
-import org.springframework.data.domain.Sort
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api/products")
-class ProductController(private val productRepository: ProductRepository) {
+class ProductController(private val service: ProductService) {
 
     @GetMapping
-    fun getProducts(): Collection<Product> = this.productRepository.findAll()
+    fun getProducts(): Collection<Product> = service.getProducts()
 
     @GetMapping("/{id}")
-    fun getProduct(@PathVariable id: Int): Product{
-        val foundProducts = this.productRepository.findAllById(listOf(id))
-
-        if(foundProducts.size==0) throw NoSuchElementException("Could not find a Product with ID=$id")
-        else return foundProducts[0]
-    }
+    fun getProduct(@PathVariable id: Int): Product = service.getProduct(id)
 
     @GetMapping("/backend")
     fun getBackendProducts(
         @RequestParam("s", defaultValue = "") s: String,
         @RequestParam("sort", defaultValue = "") sort: String,
         @RequestParam("page", defaultValue = "1") page: Int
-    ): Any {
-        val direction = when (sort) {
-            "asc" -> Sort.by(Sort.Direction.ASC, "price")
-            "desc" -> Sort.by(Sort.Direction.DESC, "price")
-            else -> Sort.unsorted()
-        }
-
-        val sizePerPage = 10
-        val total = this.productRepository.countSearch(s)
-
-        return PaginatedResponse(
-            data = this.productRepository.search(s, PageRequest.of(page - 1, sizePerPage, direction)),
-            total,
-            sizePerPage,
-            page,
-            totalPages = (total / sizePerPage) + 1
-        )
-    }
+    ): Any = service.getBackendProducts(s, sort, page)
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    fun addProduct(@RequestBody product: Product): Product {
-
-        val products = this.productRepository.findAll()
-        if (products.any { it.id == product.id }) {
-            throw IllegalArgumentException("Product with ID ${product.id} already exists.")
-        }
-
-        return this.productRepository.save(product)
-    }
+    fun addProduct(@RequestBody product: Product): Product = service.addProduct(product)
 
     @ExceptionHandler(NoSuchElementException::class)
     fun handleNotFound(e: NoSuchElementException): ResponseEntity<String> =
---
+++ src/main/kotlin/com/focus617/webbackendspringboot/domain/interactors/ProductService.kt
@@ -0,0 +1,51 @@
+package com.focus617.webbackendspringboot.domain.interactors
+
+import com.focus617.webbackendspringboot.data.dtos.PaginatedResponse
+import com.focus617.webbackendspringboot.domain.model.Product
+import com.focus617.webbackendspringboot.domain.repository.ProductRepository
+import org.springframework.data.domain.PageRequest
+import org.springframework.data.domain.Sort
+import org.springframework.stereotype.Service
+
+@Service
+class ProductService(private val productRepository: ProductRepository) {
+
+    fun getProducts(): Collection<Product> = productRepository.findAll()
+
+    fun getProduct(id: Int): Product {
+        val foundProducts = productRepository.findAllById(listOf(id))
+
+        if (foundProducts.size == 0) throw NoSuchElementException("Could not find a Product with ID=$id")
+        else return foundProducts[0]
+    }
+
+    fun getBackendProducts(s: String, sort: String, page: Int): Any {
+        val direction = when (sort) {
+            "asc" -> Sort.by(Sort.Direction.ASC, "price")
+            "desc" -> Sort.by(Sort.Direction.DESC, "price")
+            else -> Sort.unsorted()
+        }
+
+        val sizePerPage = 10
+        val total = productRepository.countSearch(s)
+
+        return PaginatedResponse(
+            data = productRepository.search(s, PageRequest.of(page - 1, sizePerPage, direction)),
+            total,
+            sizePerPage,
+            page,
+            totalPages = (total / sizePerPage) + 1
+        )
+    }
+
+    fun addProduct(product: Product): Product {
+
+        val products = productRepository.findAll()
+        if (products.any { it.id == product.id }) {
+            throw IllegalArgumentException("Product with ID ${product.id} already exists.")
+        }
+
+        return productRepository.save(product)
+    }
+
+}
