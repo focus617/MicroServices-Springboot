6ace8342fc31c521e033ba1062187d6984b348d7
focus617 <13980668829@163.com>
2022-09-10 11:34:19 +0800

    Refactor PaginatedResponse as data class.

--- src/main/kotlin/com/focus617/webbackendspringboot/data/dtos/PaginatedResponse.kt
+++ src/main/kotlin/com/focus617/webbackendspringboot/data/dtos/PaginatedResponse.kt
@@ -1,9 +1,8 @@
 package com.focus617.webbackendspringboot.data.dtos
 
-class PaginatedResponse(
+data class PaginatedResponse(
     val data: List<Any>,
     val total: Int,
     val page: Int,
     val last_page: Int
-) {
-}
+)
