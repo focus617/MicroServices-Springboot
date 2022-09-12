e390481b7ce30c7714a86805b90a344156dcc172
focus617 <13980668829@163.com>
2022-09-12 17:22:58 +0800

    Upgrade Build_gradle to introducing Spring Cloud.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,15 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
+buildscript {
+    dependencies {
+        classpath("io.spring.gradle:dependency-management-plugin:1.0.10.RELEASE")
+    }
+}
+
+ext {
+    set("springCloudVersion", "Hoxton.SR8")
+}
+
 plugins {
     id("org.springframework.boot") version "2.7.3"
     id("io.spring.dependency-management") version "1.0.13.RELEASE"
@@ -7,12 +17,24 @@ plugins {
     kotlin("plugin.spring") version "1.6.21"
 }
 
+repositories {
+    mavenCentral()
+}
+
 group = "com.focus617"
 version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_17
 
-repositories {
-    mavenCentral()
+var springCloudVersion = "2021.0.4"
+var assertJCoreVersion = "3.23.1"
+var mockkVersion = "1.12.7"
+
+apply(plugin = "io.spring.dependency-management")
+
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:${springCloudVersion}")
+    }
 }
 
 dependencies {
@@ -21,8 +43,8 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     // Test
     testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("org.assertj:assertj-core:3.23.1")
-    testImplementation("io.mockk:mockk:1.12.7")
+    testImplementation("org.assertj:assertj-core:${assertJCoreVersion}")
+    testImplementation("io.mockk:mockk:${mockkVersion}")
 }
 
 tasks.withType<Test> {
