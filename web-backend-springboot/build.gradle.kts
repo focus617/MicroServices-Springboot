61114c45a30ebaa528c2e70500c390ab46b918be
focus617 <13980668829@163.com>
2022-09-13 15:13:22 +0800

    Trial on Thymeleaf - html template solution.

--- web-backend-springboot/build.gradle.kts
+++ web-backend-springboot/build.gradle.kts
@@ -20,6 +20,7 @@ var mockkVersion = "1.12.7"
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
---
+++ web-backend-springboot/src/main/kotlin/com/focus617/webbackendspringboot/controller/ThymeleafController.kt
@@ -0,0 +1,16 @@
+package com.focus617.webbackendspringboot.controller
+
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.GetMapping
+
+@Controller
+class ThymeleafController {
+
+    @GetMapping("/index")
+    fun index(model: Model): String {
+        model.addAttribute("welcome", "This is test page for Thymeleaf.")
+        return "index"
+    }
+
+}
